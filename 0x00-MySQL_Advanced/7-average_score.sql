a7cd60b50c21e9af51da8b721c3b03adff295423
CREDO23 <thierrybakera12@gmail.com>
2023-12-14 09:27:09 +0200

    refact: refactor solution 7[

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,34 +1,25 @@
--- Creates a stored procedure ComputeAverageScoreForUser that
--- computes and stores the average score for a student.
-DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+-- SQL script that creates a stored procedure ComputeAverageScoreForUser
+-- computes and store the average score for a student
+-- An average score can be a decimal
+-- Procedure ComputeAverageScoreForUser is taking 1 input:
+-- user_id, a users.id value (you can assume user_id is linked to an existing users)
+DROP procedure IF EXISTS ComputeAverageScoreForUser;
 
-DELIMITER $ $ CREATE PROCEDURE ComputeAverageScoreForUser (user_id INT) BEGIN DECLARE total_score INT DEFAULT 0;
-
-DECLARE projects_count INT DEFAULT 0;
-
-SELECT
-    SUM(score) INTO total_score
-FROM
-    corrections
-WHERE
-    corrections.user_id = user_id;
-
-SELECT
-    COUNT(*) INTO projects_count
-FROM
-    corrections
-WHERE
-    corrections.user_id = user_id;
-
+DELIMITER | CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT) BEGIN
 UPDATE
     users
 SET
-    users.average_score = IF(
-        projects_count = 0,
-        0,
-        total_score / projects_count
+    average_score =(
+        SELECT
+            AVG(score)
+        FROM
+            corrections
+        WHERE
+            corrections.user_id = user_id
     )
 WHERE
-    users.id = user_id;
+    id = user_id;
 
-END $ $ DELIMITER;
+END;
+
+|
