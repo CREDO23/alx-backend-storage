ec55deba6bf82867829acbc2e8de80cc96ab657d
CREDO23 <thierrybakera12@gmail.com>
2023-12-14 09:30:58 +0200

    refact: refactor solution 7

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,15 +1,13 @@
--- SQL script that creates a stored procedure ComputeAverageScoreForUser
--- computes and store the average score for a student
--- An average score can be a decimal
--- Procedure ComputeAverageScoreForUser is taking 1 input:
--- user_id, a users.id value (you can assume user_id is linked to an existing users)
-DROP procedure IF EXISTS ComputeAverageScoreForUser;
+-- Create stored procedure ComputeAverageScoreForUser
+-- computes and stores average score for a student
+-- Takes 1 input user_id, a users.id
+DELIMITER $ $ DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 
-DELIMITER | CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT) BEGIN
+CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT) BEGIN
 UPDATE
     users
 SET
-    average_score =(
+    average_score = (
         SELECT
             AVG(score)
         FROM
@@ -20,6 +18,4 @@ SET
 WHERE
     id = user_id;
 
-END;
-
-|
+END $ $ DELIMITER;
